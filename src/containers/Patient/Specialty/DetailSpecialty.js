2b008f2ae7bcfad39fe094e2097649afbb15e256
Nam <ptnnamaxe@gmail.com>
2022-06-25 20:00:35 +0700

    video 93

--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -3,13 +3,15 @@ import { connect } from "react-redux";
 import './DetailSpecialty.scss'
 import { FormattedMessage } from 'react-intl'
 import HomeHeader from '../../HomePage/HomeHeader';
-
+import DoctorSchedule from '../Doctor/DoctorSchedule';
+import DoctorExtraInfor from '../Doctor/DoctorExtraInfor';
+import ProfileDoctor from '../Doctor/ProfileDoctor';
 
 class DetailSpecialty extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            arrDoctorId: [49, 45, 44]
         }
     }
     async componentDidMount() {
@@ -27,14 +29,44 @@ class DetailSpecialty extends Component {
 
     render() {
         let { language } = this.props
+        let { arrDoctorId } = this.state
 
         return (
-            <React.Fragment>
+            <div className='detail-specialty-container'>
                 <HomeHeader />
-                <div className='doctor-extra-infor-container'>
-                    Hello world from DetailSpecialty
+                <div className='detail-specialty-body'>
+
+                    <div className='description-specialty'>
+                        Hello world from DetailSpecialty
+                    </div>
+
+                    {arrDoctorId && arrDoctorId.length > 0 &&
+                        arrDoctorId.map((item, index) => {
+                            return (
+                                <div className='each-doctor' key={index} >
+                                    <div className='dt-content-left'>
+                                        <div className='profile-doctor'>
+                                            <ProfileDoctor
+                                                doctorId={item}
+                                                isShowDescriptionDoctor={true}
+                                            // dataTime={dataTime}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='dt-content-right'>
+                                        <DoctorSchedule
+                                            doctorIdFromParent={item}
+                                        />
+                                        <DoctorExtraInfor
+                                            doctorIdFromParent={item}
+                                        />
+                                    </div>
+                                </div>
+                            )
+                        })}
+
                 </div>
-            </React.Fragment>
+            </div>
         );
     }
 }
